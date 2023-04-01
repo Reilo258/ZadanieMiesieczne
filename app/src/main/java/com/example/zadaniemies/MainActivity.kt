5b35ed6e07408ee8cc56cff7082f5d848d3c6a71
Olivier van Overveld <olivier258@wp.pl>
2023-04-01 02:17:12 +0200

    stworzenie wyglądu toolbara

--- app/src/main/java/com/example/zadaniemies/MainActivity.kt
+++ app/src/main/java/com/example/zadaniemies/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.zadaniemies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide() // ukrycie toolbara
+
+
+        // przejscie do nowego activity
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val intent = Intent(this, info_uzytk::class.java)
+            startActivity(intent)
+        }
     }
 }
